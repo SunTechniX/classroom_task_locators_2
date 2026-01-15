e275fe97fe72eeaaa9b929767d8bf198fcb47a33
SunTechniX <110715341+SunTechniX@users.noreply.github.com>
2026-01-15 16:06:20 +0400

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -1,10 +1,10 @@
 # Задание 1: Локаторы для кнопок на https://demoqa.com/buttons
 
-DOUBLE_CLICK_CSS = ""
-DOUBLE_CLICK_XPATH = ""
+DOUBLE_CLICK_CSS = "#doubleClickBtn"
+DOUBLE_CLICK_XPATH = '//*[@id="doubleClickBtn"]'
 
-RIGHT_CLICK_CSS = ""
-RIGHT_CLICK_XPATH = ""
+RIGHT_CLICK_CSS = "#rightClickBtn"
+RIGHT_CLICK_XPATH = '//*[@id="doubleClickBtn"]'
 
 CLICK_ME_CSS = ""
-CLICK_ME_XPATH = ""
+CLICK_ME_XPATH = "//*[text()="Click Me"]"
