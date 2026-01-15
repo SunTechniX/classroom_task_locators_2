c4ca7114ada79dec6ea8326883ab42415d82f52b
SunTechniX <110715341+SunTechniX@users.noreply.github.com>
2026-01-15 15:48:31 +0400

    Add files via upload

---
+++ task_01.py
@@ -0,0 +1,10 @@
+# Задание 1: Локаторы для кнопок на https://demoqa.com/buttons
+
+DOUBLE_CLICK_CSS = ""
+DOUBLE_CLICK_XPATH = ""
+
+RIGHT_CLICK_CSS = ""
+RIGHT_CLICK_XPATH = ""
+
+CLICK_ME_CSS = ""
+CLICK_ME_XPATH = ""
---
+++ task_02.py
@@ -0,0 +1,4 @@
+# Задание 2: Точные локаторы для ВТОРОЙ ссылки "Home" на https://demoqa.com/links
+
+SECOND_LINK_CSS = ""
+SECOND_LINK_XPATH = ""
